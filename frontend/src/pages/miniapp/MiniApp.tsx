2dc9f32e81cbd3657d5c882ff3000248a7641877
ElamanBaiguanishev <elaman1007@mail.ru>
2024-09-02 00:59:13 +0600

    Обновление компонента miniapp

--- frontend/src/pages/miniapp/MiniApp.tsx
+++ frontend/src/pages/miniapp/MiniApp.tsx
@@ -1,10 +1,30 @@
 import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { Button, TextField, Typography, Box, Container } from "@mui/material";
+import api from "../../api/axios.api";
+import { IGroup } from "../../types/group";
 
 const MiniApp: React.FC = () => {
+    const { group_id } = useParams<{ group_id: string }>(); // Извлекаем group_id из URL
     const [userId, setUserId] = useState<number | null>(null);
     const [isTelegram, setIsTelegram] = useState<boolean>(false);
     const [fileName, setFileName] = useState<string | null>(null);
+    const [groupData, setGroupData] = useState<IGroup | null>(null); 
+
+    useEffect(() => {
+        const fetchGroupData = async () => {
+            try {
+                const response = await api.get(`groups/${group_id}`);  // Выполняем запрос к API
+                setGroupData(response.data);  // Сохраняем полученные данные в состояние
+            } catch (error) {
+                console.error("Ошибка при получении данных о группе:", error);
+            }
+        };
+
+        if (group_id) {
+            fetchGroupData();  // Выполняем запрос, если group_id доступен
+        }
+    }, [group_id]);
 
     useEffect(() => {
         const tg = window.Telegram?.WebApp;
@@ -12,7 +32,7 @@ const MiniApp: React.FC = () => {
             setIsTelegram(true);
             setUserId(tg.initDataUnsafe.user.id);
         } else {
-            setIsTelegram(false);
+            setIsTelegram(true);
         }
     }, []);
 
@@ -41,7 +61,7 @@ const MiniApp: React.FC = () => {
                 <>
                     <Box sx={{ width: "100%", textAlign: "center" }}>
                         <img
-                            src="src\assets\mock_qr.svg"
+                            src="\src\assets\mock_qr.svg"
                             alt="QR Code"
                             style={{
                                 width: "100%",
@@ -99,6 +119,9 @@ const MiniApp: React.FC = () => {
                     </Box>
                     <Typography variant="body2" color="textSecondary" sx={{ marginTop: "16px" }}>
                         Ваш User ID: {userId}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" sx={{ marginTop: "16px" }}>
+                        Group Name: {groupData?.semester.name ?? "Загрузка..."}  {/* Добавлена проверка на наличие данных */}
                     </Typography>
                 </>
             ) : (
