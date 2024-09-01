8e72edf5b31270c4fcdccda9a5018a7de56245b8
ElamanBaiguanishev <elaman1007@mail.ru>
2024-09-02 00:59:43 +0600

    Обновление маршрутов

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -2,6 +2,10 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../components/layout/MainLayout";
 import HomePage from "../pages/home/HomePage";
 import MiniApp from "../pages/miniapp/MiniApp";
+import PriceList from "../pages/price-list/PriceList";
+import StudyModeList from "../pages/study-mode/StudyModeList";
+import CourseList from "../pages/course/CourseList";
+import SemesterList from "../pages/semester/SemesterList";
 
 export const router = createBrowserRouter([
     {
@@ -12,11 +16,27 @@ export const router = createBrowserRouter([
             {
                 index: true,
                 element: <HomePage />
-            }
+            },
+            {
+                path: 'price-list',
+                element: <PriceList />
+            },
+            {
+                path: 'study-mode',
+                element: <StudyModeList />
+            },
+            {
+                path: 'courses',
+                element: <CourseList />
+            },
+            {
+                path: 'semesters',
+                element: <SemesterList />
+            },
         ]
     },
     {
-        path: 'miniapp',
+        path: 'miniapp/:group_id',  // Добавляем параметр group_id
         element: <MiniApp />
     }
 ])
